96ebbe45afe85f61b5a395a5420ea02349d33f27
Joel Nyongesa <joelnyongesa148@gmail.com>
2025-05-04 20:23:10 +0300

    feat: added a login route and conditional statement to present the appropriate pages when a user is logged in/out

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,24 @@ import { useState } from "react";
 
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Login from "./auth/Login";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true); //Remember to set this to true.
 
-  return (
+
+  return isAuthenticated ? (
     <div className='flex h-screen overflow-hidden'>
       <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <Routes>
         <Route path="/" element={<Dashboard sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />} />
       </Routes>
     </div>
+  ) : (
+    <Routes>
+      <Route path="/*" element={<Login />} />
+    </Routes>
   )
 }
 
