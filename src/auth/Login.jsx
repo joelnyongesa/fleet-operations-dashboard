965dd21122a6b4d7c1eab3fecf41cbb5ae56f517
Joel Nyongesa <joelnyongesa148@gmail.com>
2025-05-04 14:44:30 +0300

    feat: created the Login component with the default email and password for users to sign in to the application

---
+++ src/auth/Login.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import BasiGoFleet from '../assets/basigo-home-page-kenya.jpeg';
+import authDecoration from '../assets/auth-decoration.png';
+
+function Login() {
+    const email = "demo@email.com";
+    const password = "Fleet!Demo2025&";
+  return (
+    <div className="relative flex">
+        <div className="w-full md:w-1/2">
+            <div className='min-h-[100dvh] h-full flex flex-col after:flex-1'>
+                <div className="flex-1">
+                    <div className="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+                        {/* Insert Logo Here */}
+                    </div>
+                </div>
+                <div className='max-w-sm mx-auto w-full px-4 py-8'>
+                    <h1 className="text-3xl text-slate-800 font-bold mb-6">Welcome back! ✨</h1>
+                    <p className='italic text-slate-500 text-sm mb-6'>Use the provided credentials to sign in</p>
+                    <form action="">
+                        <input type="hidden" name="" autoComplete='off' />
+                        <div className="space-y-4">
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium mb-1">
+                                    Email
+                                </label>
+                                
+                                <input type="email" name="email" id="email" required className="w-full border rounded border-slate-200 text-slate-500 px-2 py-2 focus:outline-none focus:ring-0 focus:border-slate-400" value={email} />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block text-sm font-medium mb-1">Password</label>
+                                <input type="password" name="password" id="password" required className="w-full border rounded border-slate-200 px-2 py-2 focus:outline-none focus:ring-0 focus:border-slate-400 text-slate-500" value={password} />
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-between mt-6">
+                            <div className="mr-1">
+                                <a href="" className="text-sm underline hover:no-underline">Forgot Password?
+                                </a>
+                            </div>
+                            <button type="submit" className='bg-indigo-500 text-white whitespace-nowrap ml-3 px-3 py-2 rounded hover:bg-indigo-600 hover:cursor-pointer'>Sign In</button>
+                        </div>
+                    </form>
+                    <div className="pt-5 mt-6 border-t border-slate-200">
+                        <div className="text-sm">
+                            Dont you have an Account?
+                            <a href="" className="font-medium text-indigo-500 hover:text-indigo-600 ml-2">Sign Up</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2">
+                    <img src={BasiGoFleet} alt="Authentication image" className="object-cover object-center w-full h-full" height={1024} width={760} />
+                    <img src={authDecoration} alt="" className="absolute top-1/4 left-0 -translate-x-1/2 ml-8 hidden lg:block" width={218} height={224} />
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
