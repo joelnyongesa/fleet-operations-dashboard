1b8268d36a1e6503c3785a66bea6e7fe427b8384
Joel Nyongesa <joelnyongesa148@gmail.com>
2025-05-05 20:25:09 +0300

    fix: updated the card to accept data from the parent component and render it successfully, compared to previously rendering static data

--- src/cards/CardTemplate.jsx
+++ src/cards/CardTemplate.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
 function CardTemplate({ header, content}) {
+    const renderContent = () => {
+        if(typeof content === 'string' || typeof content === 'number') {
+            return (
+                <div className="text-3xl font-bold text-slate-800">
+                    {content}
+                </div>
+            );
+        }
+
+        if (typeof content === 'object' & content !== null && !Array.isArray(content)) {
+            return (
+                <div className="text-sm italic text-slate-800 w-full">
+                    <div className="grid grid-cols-2 gap-x-4 gap-y-1">
+                        {Object.entries(content).map(([key, value]) => (
+                            <div key={key}>
+                                <div className="font-medium text-slate-600 capitalize text-left">
+                                    { key}: 
+                                <span className="font-semibold text-slate-700 ml-1">
+                                    { value }
+                                </span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            );
+        }
+
+        return <div className="text-sm text-slate-500">Invalid content</div>;
+    };
   return (
     <div className="flex flex-col col-span-full md:col-span-3 bg-white shadow-sm rounded-lg border border-slate-200 transition delay-150 duration-300 ease-in-out hover:scale-101">
-        <div className="px-5 pt-2 pb-5">
+        <div className="px-5 pt-3 pb-5">
             {/* Header */}
-            <header className="flex justify-between items-center mb-2">
+            <header className="mb-2">
                 <div className="flex items-center space-x-3">
                     <div>
                         <h2 className="font-semibold text-slate-600">{header}</h2>
@@ -14,13 +44,7 @@ function CardTemplate({ header, content}) {
             </header>
             {/* Content section */}
             <div className="flex items-start">
-                <div className="text-sm text-slate-800 mr-2 w-full">
-                    <div className="flex flex-col space-y-2">
-                        <div className="text-sm font-semibold text-slate-500">
-                            {content}
-                        </div>
-                    </div>
-                </div>
+                { renderContent() }
             </div>
         </div>
     </div>
